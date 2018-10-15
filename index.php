888328e1bdd81b97ea90d0f297ed791f34c23f24
Sergey Tsarev/ALM/KZ <sergey.tsarev@avon.com>
2018-10-15 12:22:23 +0600

    Обновил Index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 // Подключение автоконфига.
-include_once 'classes/Autoload.php'
+include_once 'classes/Autoload.php';
 
 // Подключение класса App.
 // Для того чтобы обратиться к классу App, нужно использовать пространство имен classes и имя класса, для этого используется подключение класса: use пространствоИмен\ИмяКласса.
